70f2363854eef14a528299c25af6783952786e9c
Coookei <89310401+Coookei@users.noreply.github.com>
2025-05-24 20:24:21 +0100

    Customise Radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,13 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -27,8 +23,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Theme>
+      <body className={`${geistSans.variable} antialiased`}>
+        <Theme accentColor="cyan">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
