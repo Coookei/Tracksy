8e34d4a97632a7aedf176abe2659099340efa005
Coookei <89310401+Coookei@users.noreply.github.com>
2025-05-24 20:58:03 +0100

    Add markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,37 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+"use client";
+import { Button, TextField } from "@radix-ui/themes";
+import MDEditor from "@uiw/react-md-editor";
+import { ChangeEvent, useState } from "react";
+import rehypeSanitize from "rehype-sanitize";
 
 const NewIssuePage = () => {
+  const [value, setValue] = useState<string | undefined>("");
+
+  const handleChange = (
+    value?: string | undefined,
+    event?: ChangeEvent<HTMLTextAreaElement> | undefined
+  ) => {
+    setValue(value);
+    console.log("markdown: ", value);
+  };
+
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description" />
+
+      <MDEditor
+        data-color-mode="light"
+        value={value}
+        onChange={handleChange}
+        textareaProps={{
+          placeholder: "Please describe your issue here...",
+          // maxLength: 10,
+        }}
+        previewOptions={{
+          rehypePlugins: [[rehypeSanitize]],
+        }}
+      />
+
       <Button>Submit New Issue</Button>
     </div>
   );
