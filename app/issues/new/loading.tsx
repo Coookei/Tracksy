1303ce69c7050e2b7c8e3bdf34e7db4eaaac0873
Coookei <89310401+Coookei@users.noreply.github.com>
2025-05-26 16:30:47 +0100

    Format loading skeleton for new issue page

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,16 @@
+import { Box, Button } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton height={30} />
+      <Skeleton height={180} className="mt-3 mb-3" />
+      <Button disabled={true}>Submit New Issue</Button>
+    </Box>
+  );
 };
 
 export default LoadingNewIssuePage;
