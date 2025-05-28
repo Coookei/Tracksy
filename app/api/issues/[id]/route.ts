60ccf6d3bde8aa4abd56d096dde259c54d865597
Coookei <89310401+Coookei@users.noreply.github.com>
2025-05-28 11:28:05 +0100

    Add edit issue API

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,24 @@
+import { issueSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success) return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { id } = await params;
+  if (isNaN(Number(id))) return NextResponse.json({ error: "Invalid issue" }, { status: 400 });
+
+  const issueId = parseInt(id);
+  const issue = await prisma.issue.findUnique({ where: { id: issueId } });
+  if (!issue) return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  const { title, description } = validation.data;
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: { title, description },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
