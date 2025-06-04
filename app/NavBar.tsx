a0f46cf432b0acbeabb513cc6756b1a7a117bc98
Coookei <89310401+Coookei@users.noreply.github.com>
2025-06-04 18:18:20 +0100

    Add authentication links to NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { Box } from "@radix-ui/themes";
 import classnames from "classnames";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   const links = [
     {
@@ -40,6 +43,10 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" && <Link href="/api/auth/signout">Log out</Link>}
+        {status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link>}
+      </Box>
     </nav>
   );
 };
