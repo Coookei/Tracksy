2badf5a01326d751ae83293f507a961317952af4
Coookei <89310401+Coookei@users.noreply.github.com>
2025-06-14 11:52:05 +0100

    Populate assignee select dropdown using React Query and add loading state

--- app/issues/[id]/(details)/AssigneeSelect.tsx
+++ app/issues/[id]/(details)/AssigneeSelect.tsx
@@ -1,28 +1,42 @@
 "use client";
 
+import { Skeleton } from "@/app/components";
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const response = await axios.get<User[]>("/api/users");
-      setUsers(response.data);
-    };
-    fetchUsers();
-  }, []);
+  if (error) return null;
+
+  const skeletons = [1, 2, 3];
 
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
+      {/* position="popper" allows the menu to be opened even when all the elements are disabled */}
+      <Select.Content position={isLoading ? "popper" : undefined}>
         <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          <Select.Label>{isLoading ? "Loading..." : "Suggestions"}</Select.Label>
+          {isLoading &&
+            skeletons.map((skel) => (
+              <Select.Item key={`skel-${skel}`} value={`skel-${skel}`} disabled aria-disabled>
+                <Skeleton width="10rem" />
+              </Select.Item>
+            ))}
+
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
