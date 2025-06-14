20349eff4b321a92f9ef2a7236769de7700b3d4c
Coookei <89310401+Coookei@users.noreply.github.com>
2025-06-14 16:00:49 +0100

    Add toast notifications for assignee updates

--- app/issues/[id]/(details)/AssigneeSelect.tsx
+++ app/issues/[id]/(details)/AssigneeSelect.tsx
@@ -4,7 +4,8 @@ import { Skeleton } from "@/app/components";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -19,29 +20,51 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   });
 
   const handleValueChange = async (userId: string) => {
-    axios.patch(`/api/issues/${issue.id}`, {
-      assignedToUserId: userId === "null" ? null : userId,
-    });
+    const promise = axios
+      .patch<Issue>(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId === "null" ? null : userId,
+      })
+      .then((res) => res.data);
+    sendToast(promise);
+  };
+
+  const sendToast = (promise: Promise<Issue>) => {
+    toast.promise(
+      promise,
+      {
+        loading: "Submitting changes...",
+        success: (data) =>
+          `Successfully ${data.assignedToUserId === null ? "removed" : "updated"} assignee!`,
+        error: (err: AxiosError) => `Error updating issue: ${err.message}`,
+      },
+      { style: { minWidth: "280px" } },
+    );
   };
 
   if (isLoading) return <Skeleton height="2rem" />;
   if (error) return null;
 
   return (
-    <Select.Root onValueChange={handleValueChange} defaultValue={issue.assignedToUserId || "null"}>
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="null">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        onValueChange={handleValueChange}
+        defaultValue={issue.assignedToUserId || "null"}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="null">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
