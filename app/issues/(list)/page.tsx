9fcd829493ad4f9b9bd70d70788a102bd0de733d
Coookei <89310401+Coookei@users.noreply.github.com>
2025-06-14 16:58:23 +0100

    Add functionality to issue status filtering

--- app/issues/(list)/page.tsx
+++ app/issues/(list)/page.tsx
@@ -2,9 +2,23 @@ import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: Promise<{ status: Status }>;
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const { status } = await searchParams;
+
+  const statuses = Object.values(Status);
+  const validatedStatus = statuses.includes(status) ? status : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status: validatedStatus,
+    },
+  });
 
   return (
     <div>
