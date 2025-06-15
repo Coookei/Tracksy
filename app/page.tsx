ff3dec448456193dda438863cdf1a89a9141e81b
Coookei <89310401+Coookei@users.noreply.github.com>
2025-06-15 20:40:47 +0100

    Add functionality to the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return <Pagination itemCount={100} pageSize={10} currentPage={10} />;
+interface Props {
+  searchParams: Promise<{
+    page: string;
+  }>;
 }
+
+const HomePage = async ({ searchParams }: Props) => {
+  const params = await searchParams;
+
+  const page = parseInt(params.page) || 1;
+
+  return <Pagination itemCount={100} pageSize={10} currentPage={page} />;
+};
+
+export default HomePage;
