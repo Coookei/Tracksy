34854d1c9ab74bfc9a0d245287a5b9c692e44213
Coookei <89310401+Coookei@users.noreply.github.com>
2025-08-05 18:11:44 +0100

    Enhance IssueChart component by adding tooltips and Y-axis label

--- app/IssueChart.tsx
+++ app/IssueChart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card } from "@radix-ui/themes";
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 interface Props {
   open: number;
@@ -19,10 +19,21 @@ const IssueChart = ({ open, inProgress, closed }: Props) => {
   return (
     <Card>
       <ResponsiveContainer width="100%" height={300}>
-        <BarChart data={data}>
+        <BarChart data={data} margin={{ top: 10, right: 10, bottom: 0, left: 0 }}>
           <XAxis dataKey="label" />
-          <YAxis />
+          <YAxis
+            label={{
+              value: "Number of Issues",
+              position: "insideLeft",
+              angle: -90,
+              style: { textAnchor: "middle" },
+            }}
+          />
           <Bar dataKey="value" barSize={60} fill={"var(--accent-9)"} />
+          <Tooltip
+            contentStyle={{ backgroundColor: "#fff", border: "1px solid #ccc" }}
+            cursor={{ fill: "rgba(0, 0, 0, 0.05)" }}
+          />
         </BarChart>
       </ResponsiveContainer>
     </Card>
